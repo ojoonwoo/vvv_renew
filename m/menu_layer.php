b4f4a805eeaa7c4ea8e027ac33a7f46d9f686b85
kyhfan <kyhfan@gmail.com>
2018-05-15 17:43:44 +0900

    모바일 메인 작업 중

---
+++ m/menu_layer.php
@@ -0,0 +1,44 @@
+                <div class="global-menu">
+					<div class="inner">
+						<a href="#" class="btn-close button-menu">
+							<img src="./images/close_x_black.png" alt="">
+						</a>
+						<div class="list-wrap">
+							<ul class="list">
+								<li>
+									<a href="#" class="is-active">HOME</a>
+								</li>
+								<li>
+									<a href="#">ALL VVV</a>
+								</li>
+								<li>
+									<a href="#">BEST</a>
+								</li>
+								<li>
+									<a href="#">NEW</a>
+								</li>
+								<li>
+									<a href="#">AWARDS</a>
+								</li>
+							</ul>
+						</div>
+						<div class="about-us">
+							<div class="line"></div>
+							<div class="contacts">
+								<p><span>CONTACT US</span></p>
+								<p class="tel">
+									<i></i>
+									<span>+82 (02)532-2475</span>
+								</p>
+								<p class="sns">
+									<i class="mail"></i>
+									<i class="facebook"></i>
+									<i class="instagram"></i>
+								</p>
+							</div>
+						</div>
+						<div class="copyright">
+							COPYRIGHTS©2018 Valuable Viral Video ALL RIGHT RESERVED.
+						</div>
+					</div>
+				</div>
---
+++ m/search_layer.php
@@ -0,0 +1,88 @@
+                <div class="global-search-layer">
+					<div class="bg-dark">
+						<div class="inner">
+							<button type="button" class="layer-close"></button>
+							<div class="search-wrapper">
+								<div class="wrap">
+									<div class="search-bar">
+										<input type="text">
+									</div>
+									<button type="button" class="button-refresh">새로고침</button>
+								</div>
+								<div class="wrap sortings">
+									<div class="sort-list">
+										<div class="row">
+											<div class="sort">
+												<select name="order-date" id="order-date">
+													<option disabled selected>연도</option>
+													<option>Slower</option>
+													<option>Slow</option>
+													<option>Medium</option>
+													<option>Fast</option>
+													<option>Faster</option>
+												</select>
+											</div>
+											<div class="sort">
+												<select name="order-nation" id="order-nation">
+													<option disabled selected>국가</option>
+													<option>Slower</option>
+													<option>Slow</option>
+													<option>Medium</option>
+													<option>Fast</option>
+													<option>Faster</option>
+												</select>
+											</div>
+										</div>
+										<div class="row">
+											<div class="sort">
+												<select name="order-industry" id="order-industry">
+													<option disabled selected>산업군</option>
+													<option>Slower</option>
+													<option>Slow</option>
+													<option>Medium</option>
+													<option>Fast</option>
+													<option>Faster</option>
+												</select>
+											</div>
+											<div class="sort">
+												<select name="order-genre" id="order-genre">
+													<option disabled selected>장르</option>
+													<option>Slower</option>
+													<option>Slow</option>
+													<option>Medium</option>
+													<option>Fast</option>
+													<option>Faster</option>
+												</select>
+											</div>
+										</div>
+										<div class="row">
+											<div class="sort">
+												<select name="order-awards" id="order-awards">
+													<option disabled selected>광고제</option>
+													<option>Slower</option>
+													<option>Slow</option>
+													<option>Medium</option>
+													<option>Fast</option>
+													<option>Faster</option>
+												</select>
+											</div>
+											<div class="sort">
+												<select name="order-sortby" id="order-sortby">
+													<option disabled selected>분류</option>
+													<option>Slower</option>
+													<option>Slow</option>
+													<option>Medium</option>
+													<option>Fast</option>
+													<option>Faster</option>
+												</select>
+											</div>
+										</div>
+									</div>
+									<button type="button" class="button-apply">
+										APPLY
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
