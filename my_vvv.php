371eb53b9b9d23938923cad34f648aef4db04617
kyhfan <kyhfan@gmail.com>
2018-05-21 12:43:42 +0900

    11112222

--- my_vvv.php
+++ my_vvv.php
@@ -472,9 +472,11 @@
 							// alert("덧글이 입력되었습니다.");
 							$(".follow-state a").addClass("already");
 							$(".follow-state a").html("팔로우중");
+							$(".f-wer .count").html(Number($(".f-wer .count").html()) + 1);
 						}else if (response.match("D") == "D"){
 							$(".follow-state a").removeClass("already");
 							$(".follow-state a").html("팔로우하기");
+							$(".f-wer .count").html(Number($(".f-wer .count").html()) - 1);
 						}else if (response.match("L") == "L"){
 							alert("로그인 후 이용해 주세요!");
 							location.href = "login.php?refurl=video_detail.php?idx=<?=$video_idx?>";
