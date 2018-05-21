80436f195803be5578b3b2b3304975bf0b457243
ojw <ojoonwoo@gmail.com>
2018-05-21 14:14:37 +0900

    my vvv 컬렉션 생성 팝업 작업

---
+++ js/main.js
@@ -0,0 +1,123 @@
+(function($){
+	'use strict';
+
+	var $win = $(window),
+		$doc = $(document);
+
+	window.vvv = {};
+
+	vvv.ieMode = document.documentMode;
+
+	//popup
+	vvv.popup = {
+		bind : function(){
+			$doc
+				.on('click', '[data-popup]', function(e){
+				var $this = $(this),
+					val = $this.attr('data-popup');
+
+				if (val.match('@close')){
+					vvv.popup.close($this.closest('.popup'));
+				} else {
+					vvv.popup.show($(val));
+				}
+
+				if ($this.is('a')){
+					e.preventDefault();
+				}
+			})
+				.on('click', '[data-popup-close]', function(e){
+				var $this = $(this),
+					val = $this.attr('data-popup-close');
+
+				vvv.popup.close($(val));
+
+				if ($this.is('a')){
+					e.preventDefault();
+				}
+			});
+		},
+		show : function($popup){
+			console.log($popup.length);
+			if ($popup.length){
+				var $wrap = $popup.parent(),
+					$html = $('html');
+				
+
+				if (!$wrap.hasClass('popup-wrap')){
+					$popup.wrap('<div class="popup-wrap"></div>');
+					$wrap = $popup.parent();
+				}
+
+				if (!$wrap.hasClass('is-opened')){
+					$wrap
+						.stop().show(function(){
+						$popup.trigger('afterPopupOpened', $wrap);
+					})
+						.addClass('is-opened');
+				}
+
+				if (!$html.hasClass('popup-opened')){
+					$html.addClass('popup-opened');
+				}
+
+				$popup.trigger('popupOpened', $wrap);
+			}
+		},
+		close : function($popup){
+			if ($popup.length){
+				var $wrap = $popup.parent(),
+					$html = $('html');
+
+				$wrap.stop().fadeOut(300, function(){
+					$wrap.removeClass('is-opened');
+
+					if (!$('.popup-wrap.is-opened').length){
+						$html.removeClass('popup-opened');
+					}
+
+//					$popup.trigger('afterpopupClosed', $wrap);
+				});
+
+				$popup.trigger('popupClosed', $wrap);
+			}
+		}
+	};
+	vvv.popup.bind();
+
+	// checkbox
+//	ui.checkbox = {
+//		bind : function(){
+//			$doc.on('change.uiCheckbox', '.ui-checkbox', function(e){
+//				var $this = $(this);
+//				ui.checkbox.toggleClass($this);
+//			});
+//		},
+//		toggleClass : function($target){
+//			var id = $target.attr('id'),
+//				$label = $('label[for="'+id+'"]');
+//
+//			if ($target.is(':checked')){
+//				$target.addClass('is-checked');
+//				$label.addClass('is-checked');
+//			} else {
+//				$target.removeClass('is-checked');
+//				$label.removeClass('is-checked');
+//			}
+//		},
+//		load : function(){
+//			$('.ui-checkbox').each(function(i){
+//				var $this = $(this);
+//				ui.checkbox.toggleClass($this);
+//			});
+//		}
+//	}
+//	ui.checkbox.bind();
+
+	// agree
+	
+	// ready
+//	$(function(){
+//		
+//	});
+})(jQuery);
