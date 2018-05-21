50114259c3129bc2772f535108eaf79e3a92ebb4
kyhfan <kyhfan@gmail.com>
2018-05-21 10:43:26 +0900

    11111

--- main_exec.php
+++ main_exec.php
@@ -139,11 +139,11 @@ include_once "./include/autoload.php";
 
 			$query		= "INSERT INTO view_info(v_idx, mb_idx, mb_email, view_regdate) values('".$v_idx."','".$_SESSION['ss_vvv_idx']."','".$_SESSION['ss_vvv_email']."','".date("Y-m-d H:i:s")."')";
 			$result		= mysqli_query($my_db, $query);
-print_r($query);
-			$query2		= "UPDATE video_info2 SET play_count=play_count+1 WHERE video_idx='".$v_idx."'";
+
+            $query2		= "UPDATE video_info2 SET play_count=play_count+1 WHERE video_idx='".$v_idx."'";
 			$result2	= mysqli_query($my_db, $query2);
 
-            if ($result)
+            if ($result2)
 				$flag	= "Y";
 			else
 				$flag	= "N";
