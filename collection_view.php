9e553b12a7bd00bda1d2e257a8a175d24a83471d
kyhfan <kyhfan@gmail.com>
2018-05-24 20:44:13 +0900

    2233

--- collection_view.php
+++ collection_view.php
@@ -41,7 +41,7 @@
 
 	// 회원 정보 가져오기
 	if ($_SESSION["ss_vvv_idx"])
-		$mb_query		= "SELECT * FROM member_info WHERE idx='".$_SESSION["ss_mb_idx"]."'";
+		$mb_query		= "SELECT * FROM member_info WHERE idx='".$_SESSION["ss_vvv_idx"]."'";
 	else
 		$mb_query		= "SELECT * FROM member_info WHERE idx='".$mb_idx."'";
 	
