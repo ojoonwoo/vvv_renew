85f8ccbfb5bdac58745833ab92ffbb5468d3d2e0
kyhfan <kyhfan@gmail.com>
2018-05-24 17:33:48 +0900

    4455

--- collection_view.php
+++ collection_view.php
@@ -103,7 +103,7 @@
 											<div class="anyaction">
 												<!-- is-already 클래스 토글 on/off -->
 												<button class="favor is-already"></button>
-												<!-- <button class="secret is-already"></button> -->
+												<button class="secret is-already"></button>
 												<a href="javascript:void(0)" class="link-own"><span>miniverminiverminiver</span></a>
 											</div>
 											<!--내 컬렉션이 아닐경우-->
