edf97aa7b8b038915690ac5739ff3297e03c8797
kyhfan <kyhfan@gmail.com>
2018-05-25 14:51:30 +0900

    4444

--- collection_view.php
+++ collection_view.php
@@ -33,7 +33,7 @@
 	}
 
 	$secret_flag	= "";
-	if ($collection_data["collection_secret"] == "Y")
+	if ($collection_data["collection_secret"] == "N")
 		$secret_flag	= "is-active";
 	// 컬렉션 아이템 전체 갯수 및 아이템 배열로 재 분류하기
 	$collection_item_arr	= explode(",", $collection_item_data["video_items"]);
