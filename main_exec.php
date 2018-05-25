f3976bf8a34d934df88890d2ec11082dee8e60e7
kyhfan <kyhfan@gmail.com>
2018-05-25 14:46:01 +0900

    4444

--- main_exec.php
+++ main_exec.php
@@ -200,10 +200,11 @@ include_once "./include/autoload.php";
                 }
 
             }else{
+                $video_item_txt = $v_idx;
                 $query     = "INSERT INTO collection_item_info(c_idx, m_idx, video_items, regdate) values('".$c_idx."','".$_SESSION['ss_vvv_idx']."','".$video_item_txt."','".date("Y-m-d H:i:s")."')";
                 $result    = mysqli_query($my_db, $query);
             }
-        print_r($query);
+
             if ($flag == "")
             {
                 if ($result)
