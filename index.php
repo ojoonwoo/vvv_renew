ac8fdad4b8c347d20f54cfc6bf7f02035d87c1e7
kyhfan <kyhfan@gmail.com>
2018-05-25 15:25:07 +0900

    3333

--- index.php
+++ index.php
@@ -179,7 +179,7 @@
         $yt_thumb       = "https://img.youtube.com/vi/".$yt_code_arr2[0]."/hqdefault.jpg";
 
         $title_count    = mb_strlen($recent_data["video_title"],'utf-8');
-
+print_r($title_count);
         if ($title_count > 30)
             $video_title    = substr($recent_data["video_title"],0,30)."...";
         else
