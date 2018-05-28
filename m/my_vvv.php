eb06cf9641c504263cbb46db23964ac24cfea48a
kyhfan <kyhfan@gmail.com>
2018-05-28 14:41:06 +0900

    1111

--- m/my_vvv.php
+++ m/my_vvv.php
@@ -65,7 +65,18 @@
 									<!--me, not me-->
 									<div class="wrap-user">
 										<div class="user-id">
-											<span class="u-id">MINIVER</span>
+<?
+	if ($mb_data['mb_nickname'] == "")
+	{
+?>
+											<span class="u-id"><?=$mb_data['mb_name']?></span>
+<?
+	}else{
+?>
+											<span class="u-id"><?=$mb_data['mb_nickname']?></span>
+<?
+	}
+?>
 											<a href="javascript:void(0)" class="setting">
 												<img src="./images/icon_profile_setting.png" alt="">
 											</a>
@@ -74,25 +85,42 @@
 									<div class="wrap-actions">
 										<div class="f-wer">
 											<span>팔로워</span>
-											<span class="count">10</span>
+											<span class="count"><?=$mb_data['mb_follower_count']?></span>
 										</div>
 										<div class="f-ing">
 											<span>팔로잉</span>
-											<span class="count">10</span>
+											<span class="count"><?=$mb_data['mb_following_count']?></span>
 										</div>
-										<!--my vvv일경우-->
-										<div class="f-add">
-											<button type="button">친구추가</button>
-										</div>
-										<!--my vvv일경우-->
-										<!--남의 vvv일경우-->
-<!--
-										<div class="follow-state">
-											<a href="javascript:void(0)">팔로우하기</a>
-																								<a href="javascript:void(0)" class="already">팔로우중</a>
-										</div>
--->
-										<!--남의 vvv일경우-->
+<?
+	if ($_SESSION['ss_vvv_idx'] != $my_idx)
+	{
+		// 팔로우 여부 확인
+		$follow_query		= "SELECT * FROM follow_info WHERE follow_idx='".$follow_idx."' AND follower_idx='".$_SESSION['ss_vvv_idx']."' AND follow_YN='Y'";
+		$follow_result		= mysqli_query($my_db, $follow_query);
+		$follow_count		= mysqli_num_rows($follow_result);
+		
+		if ($follow_count > 0)
+		{
+?>
+												<div class="follow-state">
+													<a href="javascript:follow_member()" class="already">팔로우중</a>
+												</div>
+<?
+		}else{
+?>													
+												<div class="follow-state">
+													<a href="javascript:follow_member()">팔로우하기</a>
+												</div>
+<?
+		}
+	}else{
+?>
+												<div class="f-add">
+													<button type="button">친구추가</button>
+												</div>
+<?		
+	}
+?>												
 									</div>
 								</div>
 							</div>
