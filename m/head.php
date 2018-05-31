7eb729f58753453dc5a102b4a7d1f1582a38e0d5
ojw <ojoonwoo@gmail.com>
2018-05-31 17:10:57 +0900

    모바일 확대 방지 메타태그 추가

--- m/head.php
+++ m/head.php
@@ -2,7 +2,7 @@
 <html lang="ko">
 	<head>
 		<meta charset="UTF-8" >
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1">
 		<title>Document</title>
 		<link rel="stylesheet" href="./css/reset.css">
 		<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
