24edd97e78652f46eeb0a83dbc46dfe3642cddff
kyhfan <kyhfan@gmail.com>
2018-05-31 16:36:57 +0900

    3333

--- index.php
+++ index.php
@@ -425,7 +425,7 @@
 	}
 
 	$doc.on('click', '#search-layer-refresh', function() {
-		console.log("search_refresh");
+		console.log($("#order-date").val());
         $("#search_keyword").val("");
         $("#order-date").val("");
         $("#order-nation").val("");
