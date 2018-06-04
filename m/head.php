f68b9d6259950f7b8bb49e617098e5c4c867ca8c
kyhfan <kyhfan@gmail.com>
2018-06-04 16:02:49 +0900

    22222

--- m/head.php
+++ m/head.php
@@ -3,13 +3,14 @@
 	<head>
 		<meta charset="UTF-8" >
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1">
-		<title>Document</title>
+		<title>Valuable Viral Video - 가치있는 바이럴 영상</title>
 		<link rel="stylesheet" href="./css/reset.css">
 		<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.2.6/css/swiper.css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.2.6/css/swiper.min.css">
 		<link rel="stylesheet" href="./css/common.css">
 		<link rel="stylesheet" href="./css/style.css">
+		<link type="image/icon" rel="shortcut icon" href="http://www.minivertising-test.com/images/vvvlogo_favicon.ico" />
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.2.6/js/swiper.min.js"></script>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
