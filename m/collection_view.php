928ce8ce331ce8d0a6aab0b353b15975ffd16e16
kyhfan <kyhfan@gmail.com>
2018-06-07 12:39:46 +0900

    111222

--- m/collection_view.php
+++ m/collection_view.php
@@ -153,7 +153,7 @@
 											<div class="list-container">
 												<div class="video-list">
 <?
-	if ($$collection_item_data["video_items"] != "")
+	if ($collection_item_data["video_items"] != "")
 	{
 		foreach($collection_item_arr as $key => $val)
 		{
