c2dcb277ce5b7dec422ea8e9b3966e9f425bf1f1
kyhfan <kyhfan@gmail.com>
2018-06-07 12:42:04 +0900

    5555

--- m/my_vvv_popup.php
+++ m/my_vvv_popup.php
@@ -915,7 +915,8 @@
 					videoItems += $(this).val();
 					i++;
 				});
-
+console.log(videoItems);
+return false;
 				if (videoItems == "")
 				{
 					location.href = "collection_view.php?cidx=<?=$collection_idx?>&midx=<?=$mb_idx?>&my=<?=$_REQUEST["my"]?>";
